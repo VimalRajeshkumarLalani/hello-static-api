c2294cafc42e0ea4fdb791b6a89120a34dab3544
Vimal Lalani <v-vlalani@microsoft.com>
2025-07-24 16:00:14 +0530

    Update index.js

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,13 +1,11 @@
-module.exports = async function (context, req) {
-    const requestOrigin = req.headers.get("origin");
-	
+module.exports = async function (context, req) {   
     context.res = {
          headers: {
-            'Access-Control-Allow-Origin': requestOrigin,
+            'Access-Control-Allow-Origin': "*",
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         },
         // status: 200, /* Defaults to 200 */
-        body: { "data" : "Hello World from Azure Static Web Apps API!" }
+        body: { "data" : "Hello World from Azure Static Web Apps API!" , "origin": req.headers.get("origin") }
     };
 };
