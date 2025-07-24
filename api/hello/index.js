1fbd6ff415908596ee2eee5ed22e316ef90027d7
Vimal Lalani <v-vlalani@microsoft.com>
2025-07-24 15:50:05 +0530

    Update index.js

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,7 +1,9 @@
 module.exports = async function (context, req) {
+    const requestOrigin = req.headers.get("origin");
+	const origin = allowedOrigins.includes(requestOrigin) ? requestOrigin : "*";
     context.res = {
          headers: {
-            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': origin,
             'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization'
         },
