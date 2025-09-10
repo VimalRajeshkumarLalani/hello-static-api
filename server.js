d4bd64d8dfa6c99a5ab3b67b2a811a10817709c0
Vimal Lalani <v-vlalani@microsoft.com>
2025-09-10 17:00:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200; // OK
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World BranchA\n');
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
